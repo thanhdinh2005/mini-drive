e69e504ba23242e3278c98d7756a29c09fd6c007
thanhknghien <thanh2005dinh@gmail.com>
2026-09-20 16:39:47 +0700

    fix: modify dependency at TestContainer

--- backend/src/test/java/com/spring/backend/TestcontainersConfiguration.java
+++ backend/src/test/java/com/spring/backend/TestcontainersConfiguration.java
@@ -3,16 +3,13 @@ package com.spring.backend;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.testcontainers.postgresql.PostgreSQLContainer;
-import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
-
-	@Bean
-	@ServiceConnection
-	PostgreSQLContainer postgresContainer() {
-		return new PostgreSQLContainer(DockerImageName.parse("postgres:latest"));
-	}
-
+  @Bean
+  @ServiceConnection
+  PostgreSQLContainer<?> postgres() {
+    return new PostgreSQLContainer<>("postgres:17");
+  }
 }
